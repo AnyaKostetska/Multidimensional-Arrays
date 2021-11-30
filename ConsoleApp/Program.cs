a1190c9a1f4473d598676aeed27abb4d6ae73bb9
annkostet <annakostetska30@gmail.com>
2021-11-30 22:04:25 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MultidimensionalArraysLibrary;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] array = new int[3, 4];
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(1, 20);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Multidimensional Arrays/Array_Helper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace MultidimensionalArraysLibrary
+{
+    public class Array_Helper
+    {
+        public static int FindMinimumValueInArray(int[,] array)
+        {
+            int min = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        public static int FindMaximumValueInArray(int[,] array)
+        {
+            int max = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        public static (int, int) ToFindIndexOfMaxElement(int[,] array)
+        {
+            int max = array[0, 0];
+            int maxi = 0;
+            int maxj = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; 0 < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > max)
+                    {
+                        maxi = i;
+                        maxj = j;
+                    }
+                }
+            }
+
+            return (maxi, maxj);
+
+        public static (int, int) ToFindIndexOfMinElement(int[,] array)
+        {
+            int min = array[0, 0];
+            int mini = 0;
+            int minj = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; 0 < array.GetLength(1); j++)
+                    {
+                        if (array[i, j] < min)
+                        {
+                            mini = i;
+                            minj = j;
+                        }
+                    }
+                }
+
+                return (mini, minj);
+            }
+    }
+}
